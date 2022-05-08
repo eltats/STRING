139610f246fae8c6c971a0d559dc7a5f9d5c02f8
User <lesya@MacBook-Air-Olesa.local>
2022-05-08 18:48:51 +0300

    s

--- s21_string.h
+++ s21_string.h
@@ -15,7 +15,14 @@ typedef struct s_flags
 	int precision;
 	short int pr;
 }			t_flags;
-int s21_memcmp(const void *str1, const void *str2, size_t n);
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
 int is_digit(int c);
 char *s21_strcat(char *dest, const char *src);
-char *s21_strncat(char *dest, const char *src, size_t n);
+char *s21_strncat(char *dest, const char *src, s21_size_t n);
+void *s21_memmove(void *dest, const void *src, s21_size_t n);
+int s21_sprintf(char *str,  char *format, ...);
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
+char *s21_strncpy(char *dest, const char *src, s21_size_t n);
+char *s21_strpbrk(const char *str1, const char *str2);
+s21_size_t s21_strspn(const char *str1, const char *str2);
+char *s21_strtok(char *str, const char *denim);
--- s21_strspn.c
+++ s21_strspn.c
@@ -1,8 +1,8 @@
 #include "s21_string.h"
 
-size_t s21_strspn(const char *str1, const char *str2)
+s21_size_t s21_strspn(const char *str1, const char *str2)
 {
-	size_t res = 0;
+	s21_size_t res = 0;
 
 	while (str1[res] && str1[res] == str2[res])
 		res++;
